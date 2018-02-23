fbaf1fe09d9d97a91c8e4eb73ac8158354480ec8
Bryan <d58526@gmail.com>
2018-02-23 14:39:49 +0800

    Ch4 Example

---
+++ TheArtOfUnitTestingPractice.UnitTests/Ch3/LogAnalyzerTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using TheArtOfUnitTestingPractice.Ch3;
+
+namespace TheArtOfUnitTestingPractice.UnitTests.Ch3
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_NameSupportedExtension_ReturnsTrue()
+        {
+            FakeExtensionManager myFakeExtensionManager = new FakeExtensionManager();
+            myFakeExtensionManager.WillBeValid = true;
+            LogAnalyzer log = new LogAnalyzer(myFakeExtensionManager);
+            var result = log.IsValidLogFileName("short.txt");
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidFileName_ExtManagerThrowsException_ReturnsFalse()
+        {
+            FakeExtensionManager myFakeExtensionManager = new FakeExtensionManager();
+            myFakeExtensionManager.WillThrow = new Exception("this is fake");
+            LogAnalyzer log = new LogAnalyzer(myFakeExtensionManager);
+            var result = log.IsValidLogFileName("anything.anyextension");
+            Assert.False(result);
+        }
+
+        [Test]
+        public void OverrideTest()
+        {
+            FakeExtensionManager stub = new FakeExtensionManager();
+            stub.WillBeValid = true;
+
+            TestableLogAnalyzer logan = new TestableLogAnalyzer(stub);
+            var result = logan.IsValidLogFileName("file.ext");
+            Assert.True(result);
+        }
+    }
+
+    public class LogAnalyzerUsingFactoryMethod
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            return GetManager().IsValid(fileName);
+        }
+
+        protected virtual IFileExtensionManager GetManager()
+        {
+            return new FileExtensionManager();
+        }
+    }
+
+    public class TestableLogAnalyzer : LogAnalyzerUsingFactoryMethod
+    {
+        public IFileExtensionManager Manager;
+
+        public TestableLogAnalyzer(FakeExtensionManager mgr)
+        {
+            this.Manager = mgr;
+        }
+
+        protected override IFileExtensionManager GetManager()
+        {
+            return this.Manager;
+        }
+    }
+
+    public class FakeExtensionManager : IFileExtensionManager
+    {
+        public bool WillBeValid { get; set; }
+
+        public Exception WillThrow { get; set; }
+
+        public bool IsValid(string fileName)
+        {
+            if (WillThrow != null)
+            {
+                throw WillThrow;
+            }
+            return WillBeValid;
+        }
+    }
+}
---
+++ TheArtOfUnitTestingPractice/Ch3/LogAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheArtOfUnitTestingPractice.Ch3
+{
+    public class LogAnalyzer
+    {
+        private IFileExtensionManager manager;
+
+        public LogAnalyzer(IFileExtensionManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public LogAnalyzer()
+        {
+            this.manager = new FileExtensionManager();
+        }
+
+        public IFileExtensionManager ExtensionManager
+        {
+            get
+            {
+                return this.manager;
+            }
+            set
+            {
+                this.manager = value;
+            }
+        }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            return this.manager.IsValid(fileName);
+        }
+    }
+
+    public interface IFileExtensionManager
+    {
+        bool IsValid(string fileName);
+    }
+
+    public class FileExtensionManager : IFileExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            // read some file here
+            return true;
+        }
+    }
+
+    public class AlwaysValidFakeExtensionManager : IFileExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            return true;
+        }
+    }
+}
